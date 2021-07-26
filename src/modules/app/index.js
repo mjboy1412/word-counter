3d03b9d2d5e9877058cd3b586bb7c04f1e60a27b
Ha Nguyen <mjboy1412@gmail.com>
2021-07-26 10:34:24 -1000

    init

---
+++ src/modules/app/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { StyledDivApp, StyledDivHeader, StyledTextArea } from "./styles";
+
+const App = () => {
+  return (
+    <StyledDivApp>
+      <StyledDivHeader></StyledDivHeader>
+      <StyledTextArea></StyledTextArea>
+    </StyledDivApp>
+  );
+};
+
+export default App;
---
+++ src/modules/app/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const StyledDivApp = styled.div`
+  height: 700px;
+  width: 700px;
+  border: 1px solid black;
+`;
+
+export const StyledDivHeader = styled.div`
+  height: 50px;
+`;
+
+export const StyledTextArea = styled.textarea`
+  height: 80%;
+  border: none;
+  outline: none;
+  resize: none;
+`;
+
+export const StyledFooter = styled.div`
+  height: 50px;
+`;
