f91c40e584e3f0558f153e9f3c7a243639030c00
Ha Nguyen <mjboy1412@gmail.com>
2021-07-27 05:41:27 +0700

    create and styling header

--- src/modules/app/index.js
+++ src/modules/app/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-import { StyledDivApp, StyledDivHeader, StyledTextArea } from "./styles";
+import {
+  StyledXIcon,
+  StyledDivApp,
+  StyledDivHeader,
+  StyledTextArea,
+  StyledButtonTweet,
+} from "./styles";
 
 const App = () => {
   return (
     <StyledDivApp>
-      <StyledDivHeader></StyledDivHeader>
-      <StyledTextArea></StyledTextArea>
+      <StyledDivHeader>
+        <StyledXIcon />
+        <StyledButtonTweet>Tweet</StyledButtonTweet>
+      </StyledDivHeader>
+      <StyledTextArea placeholder="Remember, be nice!"></StyledTextArea>
     </StyledDivApp>
   );
 };
--- src/modules/app/styles.js
+++ src/modules/app/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { BsX } from "react-icons/bs";
 
 export const StyledDivApp = styled.div`
   height: 700px;
@@ -7,7 +8,12 @@ export const StyledDivApp = styled.div`
 `;
 
 export const StyledDivHeader = styled.div`
-  height: 50px;
+  height: 80px;
+  padding: 15px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const StyledTextArea = styled.textarea`
@@ -15,8 +21,34 @@ export const StyledTextArea = styled.textarea`
   border: none;
   outline: none;
   resize: none;
+  width: 100%;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 16px;
+  font-family: Segoe UI;
 `;
 
 export const StyledFooter = styled.div`
   height: 50px;
+`;
+
+export const StyledXIcon = styled(BsX)`
+  font-size: 42px;
+  color: #539bf5;
+  cursor: pointer;
+`;
+
+export const StyledButtonTweet = styled.button`
+  background: #539bf5;
+  color: #fff;
+  border-radius: 30px;
+  border: none;
+  font-size: 18px;
+  outline: none;
+  width: 94px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
 `;
